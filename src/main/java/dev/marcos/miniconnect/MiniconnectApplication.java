ce089fbd0de3b925259dd12fd8240ac72182e259
Marcos <marcosallanm1to@gmail.com>
2026-06-07 08:53:04 -0300

    first commit

---
+++ src/main/java/dev/marcos/miniconnect/MiniconnectApplication.java
@@ -0,0 +1,13 @@
+package dev.marcos.miniconnect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniconnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniconnectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/marcos/miniconnect/MiniconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.marcos.miniconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
